14b8173aa652c2d0b979681445e23d27eedadb67
Marito048 <estudio1802@gmail.com>
2022-03-12 18:58:34 -0300

    primera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaComponent } from './componentes/acerca/acerca.component';
+import { LaboralComponent } from './componentes/laboral/laboral.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { PublicidadComponent } from './componentes/publicidad/publicidad.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercaComponent,
+    LaboralComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    PublicidadComponent
   ],
   imports: [
     BrowserModule
