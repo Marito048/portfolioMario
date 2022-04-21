1471490dbf060d78ce299342d9b63718dd4c050b
Marito048 <estudio1802@gmail.com>
2022-04-21 09:45:16 -0300

    submmit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { RouterModule,Routes } from '@angular/router';
 
 
 const routes: Routes =[
-  {path: '', component: AppComponent, pathMatch: "full"},
-  
-  
+  {path: '/login', component: AppComponent, pathMatch: "full"},
+
+
 ];
 
 @NgModule({
@@ -15,7 +15,7 @@ const routes: Routes =[
   imports: [
     CommonModule
   ]
-  
+
 })
 
 
--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,5 +1,9 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AutenticacionService } from 'src/app/servicios/autenticacion.service';
+
+
 
 @Component({
   selector: 'app-login',
@@ -9,17 +13,40 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
   title = 'bootstrap-popup';
   loginForm!: FormGroup;
+  form:FormGroup
+  constructor(private formBuilder:FormBuilder, private autenticacionService:AutenticacionService, private ruta:Router){
+    this.form=this.formBuilder.group({
+      email:['',[Validators.required,Validators.email]],
+      password:['',[Validators.required,Validators.minLength(8)]],
+      deviceInfo:this.formBuilder.group({
+        deviceId:["17867868768"],
+        deviceType:["DEVICE_TYPE_ANDROID"],
+        notificationToken:["123456789"]
 
-  constructor() { }
-
-  ngOnInit(): void {
-    this.loginForm = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required])
+      })
     })
   }
 
-  get emailField(): any {
+  ngOnInit(): void {
+  }
+  get Email() {
+    return this.form.get('email');
+
+  }
+  get Password(){
+    return this.form.get('password');
+  }
+  onEnviar(event: Event){
+    event.preventDefault;
+    this.autenticacionService.IniciarSesion(this.form.value).subscribe(data=>{
+      console.log("DATA: " + JSON.stringify(data));
+      //this.ruta.navigate(['/']);
+    })
+  }
+
+
+
+  /*get emailField(): any {
     return this.loginForm.get('email');
   }
 
@@ -30,5 +57,6 @@ export class LoginComponent implements OnInit {
   loginFormSubmit(): void {
     console.log(this.loginForm.value);
     // Call Api
-  }
+  }*/
 }
+
